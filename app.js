65ebbfd515e1340d72de94c30c5bf3fb93e1148d
M Umer Aziz <m.umer394aziz@gmail.com>
2026-03-09 02:33:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Ye app me nay umer aziz ne bnai hay!")
+let listcontainer = document.querySelector(".list-container")
+let inputbox = document.querySelector("#input-box")
+let addbtn = document.querySelector(".add-btn")
+
+addbtn.addEventListener(onclick , ()=>{
+
+})
