80e8eaa9bc265984de29f5e02976b2bba099205f
M Umer Aziz <m.umer394aziz@gmail.com>
2026-03-09 06:20:40 +0500

    Add files via upload
    project is completed

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
-alert("Ye app me nay umer aziz ne bnai hay!")
-let listcontainer = document.querySelector(".list-container")
-let inputbox = document.querySelector("#input-box")
-let addbtn = document.querySelector(".add-btn")
+// alert("Ye app me nay umer aziz ne bnai hay!")
+let listContainer = document.querySelector(".list-container");
+let inputBox = document.querySelector(".input-box");
+let addBtn = document.querySelector(".add-btn");
 
-addbtn.addEventListener(onclick , ()=>{
+inputBox.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addBtn.click();
+    }
+});
+addBtn.addEventListener('click', () => {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+     saveData();
+});
 
-})
+listContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+         saveData();
+    }
+    else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+         saveData();
+    }
+
+});
+function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML)
+}
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data")
+};
+showData();
+
+
